f11659782d8914431660a6aa5bc95a95d1a4d32f
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-12-31 17:53:53 +0100

    signup form

--- src/App.js
+++ src/App.js
@@ -28,6 +28,32 @@ const theme = createMuiTheme({
   },
   typography: {
     useNextVariants: true
+  },
+  styles: {
+    form: {
+      textAlign: 'center'
+    },
+    image: {
+      margin: '20px auto'
+    },
+    pageTitle: {
+      margin: '10px auto'
+    },
+    textField: {
+      margin: '10px auto'
+    },
+    button: {
+      marginTop: 20,
+      position: 'relative'
+    },
+    customError: {
+      color: 'red',
+      fontSize: '0.8rem',
+      marginTop: 10
+    },
+    progress: {
+      position: 'absolute'
+    }
   }
 });
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -13,32 +13,9 @@ import {
   withStyles
 } from '@material-ui/core'
 
-const styles = {
-  form: {
-    textAlign: 'center'
-  },
-  image: {
-    margin: '20px auto'
-  },
-  pageTitle: {
-    margin: '10px auto'
-  },
-  textField: {
-    margin: '10px auto'
-  },
-  button: {
-    marginTop: 20,
-    position: 'relative'
-  },
-  customError: {
-    color: 'red',
-    fontSize: '0.8rem',
-    marginTop: 10
-  },
-  progress: {
-    position: 'absolute'
-  }
-}
+const styles = (theme) => ({
+  ...theme.styles
+})
 
 class login extends Component {
   constructor() {
@@ -63,6 +40,7 @@ class login extends Component {
     axios.post('/login', userData)
       .then(res => {
         console.log(res.data)
+        localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`)
         this.setState({
           loading: false
         })
