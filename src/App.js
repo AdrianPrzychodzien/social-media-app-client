75e2932fe3f0f711e1e244f4e0371af096079996
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-12-31 14:19:46 +0100

    application connected to firebase, first screams fetched, component to display comments created with material-UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 // Components
 import NavBar from './components/NavBar'
@@ -10,23 +11,45 @@ import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Router>
-            <NavBar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={home} />
-                <Route exact path="/login" component={login} />
-                <Route exact path="/signup" component={signup} />
-              </Switch>
-            </div>
-          </Router>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <Router>
+              <NavBar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={home} />
+                  <Route exact path="/login" component={login} />
+                  <Route exact path="/signup" component={signup} />
+                </Switch>
+              </div>
+            </Router>
+          </header>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
