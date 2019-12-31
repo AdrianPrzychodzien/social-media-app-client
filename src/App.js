4db86f4e3629f08d80494ba5924f270578d01b61
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-12-31 19:20:31 +0100

    token authentication for users added

--- src/App.js
+++ src/App.js
@@ -2,60 +2,31 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import themeFile from './util/theme'
+import jwtDecode from 'jwt-decode'
 
 // Components
 import NavBar from './components/NavBar'
+import AuthRoute from './util/AuthRoute'
 
 // Pages
 import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
-    },
-  },
-  typography: {
-    useNextVariants: true
-  },
-  styles: {
-    form: {
-      textAlign: 'center'
-    },
-    image: {
-      margin: '20px auto'
-    },
-    pageTitle: {
-      margin: '10px auto'
-    },
-    textField: {
-      margin: '10px auto'
-    },
-    button: {
-      marginTop: 20,
-      position: 'relative'
-    },
-    customError: {
-      color: 'red',
-      fontSize: '0.8rem',
-      marginTop: 10
-    },
-    progress: {
-      position: 'absolute'
-    }
+const theme = createMuiTheme(themeFile);
+
+let authenticated
+const token = localStorage.FBIdToken
+if (token) {
+  const decodedToken = jwtDecode(token)
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login'
+    authenticated = false
+  } else {
+    authenticated = true
   }
-});
+}
 
 class App extends Component {
   render() {
@@ -68,8 +39,16 @@ class App extends Component {
               <div className="container">
                 <Switch>
                   <Route exact path="/" component={home} />
-                  <Route exact path="/login" component={login} />
-                  <Route exact path="/signup" component={signup} />
+                  <AuthRoute
+                    exact path="/login"
+                    component={login}
+                    authenticated={authenticated}
+                  />
+                  <AuthRoute
+                    exact path="/signup"
+                    component={signup}
+                    authenticated={authenticated}
+                  />
                 </Switch>
               </div>
             </Router>
